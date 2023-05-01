0f2648208125c07b0882a7ccf2c72cce72fd93e1
Chungxhung <loveiesschung98@gmail.com>
2023-05-01 14:24:19 +0900

    lucky number selector python

---
+++ selector.py
@@ -0,0 +1,9 @@
+# lucky-num-selector
+
+num = int(input("몇번 반복할까요? :")) # 반복할 횟수
+
+for i in range(num): # num번 반복하기
+    
+    for i in range(6): # 1부터 45까지 숫자에서 6개 숫자 뽑기
+        print(random.randint(1,46))
+    print("===")
