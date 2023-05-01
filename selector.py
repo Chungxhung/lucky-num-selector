a77556e0b5171375933c683cc51889d7db1adda0
Chungxhung <loveiesschung98@gmail.com>
2023-05-01 14:40:09 +0900

    selector fixed as function

--- selector.py
+++ selector.py
@@ -1,11 +1,10 @@
-# lucky-num-selector
+import random
 
-import random # random 함수 불러오기
 
-num = int(input("몇번 반복할까요? :")) # 반복할 횟수
+def get_lucky_nums():
+    return random.sample(range(1,45+1), k=6)
 
-for i in range(num): # num번 반복하기
-    
-    for i in range(6): # 1부터 45까지 숫자에서 6개 숫자 뽑기
-        print(random.randint(1,46))
-    print("===")
+
+if __name__ == '__main__':
+    print(get_lucky_nums())# lucky-num-selector
+
